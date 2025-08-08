8078e68bb7baf4e973846fd6714c52c2f0b9e516
https-helios <guan.s@oundleschool.org.uk>
2025-08-08 11:47:30 +0100

    taking a break

--- scriptTest.js
+++ scriptTest.js
@@ -22,9 +22,21 @@ function appendNumber(number){ //generating code for outputting the numbers onto
 }
 
 function toggleSign(){
+    //working on this currently
     sign = sign ? false : true; //toggle the true and false after every press VERY IMPORTANT LINE OF CODE 
     document.getElementById('positive-negative').innerHTML = sign ? '-' : '+'; //If the sign is true then display negative, vice versa.
     console.log('sign toggled to: ', `${sign}`);
+    if (sign){
+        appendNumber('-'); //if the sign is true, then append a negative sign
+    }
+    else{
+        //if the sign is false, then do nothing
+        //this is because we don't want to append a positive sign
+        //the positive sign is implicit
+        console.log('Positive sign not appended');
+    }
+    sign = false; //reset the sign after appending the number
+    //find a way to reset the function from here
 }
 
 function appendOperation(operation){
@@ -96,6 +108,7 @@ function calculate(){
 
     currentOperation = '';
     currentInput = '';
+    console.log(`${sign}`);
 
 }
 
